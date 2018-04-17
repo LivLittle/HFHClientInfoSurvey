aa5665980a29ce84928386c9814774d6337efdf7
LivLittle <olittle0424@gmail.com>
2018-04-17 13:26:00 -0500

    Description and Imports

--- ExampleGUINo1/src/exampleguino1/ExampleGUINo1.java
+++ ExampleGUINo1/src/exampleguino1/ExampleGUINo1.java
@@ -1,21 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Harold Pinks, Jorge Pizarro, Frank Cutrera, Olivia Little
+ * This file is an example of how the GUI for the Habitat for humanity form will look.
+ * This file will not yet connect to the actual database
  */
 package exampleguino1;
 
-/**
- *
- * @author olittle867
- */
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class ExampleGUINo1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
-    
 }
