5d418a25961a4c39238dc0ec3480dd26b6c68eaa
LivLittle <olittle0424@gmail.com>
2018-04-19 13:33:40 -0500

    Layout change
    Still needs to be cleaned up

--- ExampleGUINo1/src/exampleguino1/ExampleGUINo1.java
+++ ExampleGUINo1/src/exampleguino1/ExampleGUINo1.java
@@ -20,20 +20,105 @@ import java.awt.*;
 
 public class ExampleGUINo1 extends JFrame {
     
-    private final ActionButtonHandler actionHandler;  //this action handler is used for ALL the buttons
-    private JFrame frame;
-    private Container container;
-    private JPanel panel;
-    private JTextField firstName, lastName, email, phoneNum1,phoneNum2, phoneNum3, street, city, state, zip, quote;
-    private JRadioButton disregardEmail, disregardPhoneNum, disregardAddress, emailOption, textOption, volunteerOption, disregardQuote, quoteYesOption, quoteNoOption;
+//    private final ActionButtonHandler actionHandler;  //this action handler is used for ALL the buttons
+//    private JFrame frame;
+//    private Container container;
+//    private JPanel panel;
+//    private Dimension d1, d2, d3, d4, d5;
+//    private JTextField firstName, lastName, email, phoneNum1,phoneNum2, phoneNum3, street, city, state, zip, quote;
+//    private JRadioButton disregardEmail, disregardPhoneNum, disregardAddress, emailOption, textOption, volunteerOption, disregardQuote, quoteYesOption, quoteNoOption;
     
-    public ExampleGUINo1() {
+    static JComponent[] components = {
+        new JTextField(15),
+        new JTextField(15),
+        new JTextField(15),
+        new JRadioButton(),
+        new JTextField(10),
+        new JRadioButton(),
+        new JTextField(),
+        new JTextField(),
+        new JTextField(),
+        new JTextField(),
+        new JRadioButton(),
+        new JRadioButton(),
+        new JRadioButton(),
+        new JRadioButton(),
+        new JTextField(),
+        new JRadioButton(),
+        new JRadioButton()
+    };
+    
+    static String[] stringLabels = {
+        "FirstName: ",
+        "LastName: ",
+        "Email Address: ",
+        "Phone Number(no dashes): ",
+        "I prefer not to give my email address: ",
+        "Street Name: ",
+        "City: ",
+        "State: ",
+        "Zip Code: ",
+        "I prefer not to give my address: ", 
+        "I want email notifications: ",
+        "I want text notifications: ",
+        "I want to be contacted about volunteer information: ",
+        "What do you like best about ReStore?",
+        "I prefer not to answer",
+        "I allow this discription to be quoted:",
+        "I do not allow this discription to be quoted:",
+    };
+    
+    public static JComponent setDualColumnFormat(String[] stringLabels, JComponent[] components) {
+        JComponent panel = new JPanel();
+        GroupLayout groupLayout = new GroupLayout(panel);
+        panel.setLayout(groupLayout);
+        groupLayout.setAutoCreateContainerGaps(true);
+        JLabel[] labels = new JLabel[stringLabels.length];
+        
+        for(int i = 0; i < labels.length; i++) {
+            labels[i] = new JLabel(stringLabels[i]);
+        }
+        
+        GroupLayout.SequentialGroup row = groupLayout.createSequentialGroup();
+        GroupLayout.Group labelColumn = groupLayout.createParallelGroup(GroupLayout.Alignment.TRAILING);
+        row.addGroup(labelColumn);
+        
+        GroupLayout.Group componentColumn = groupLayout.createParallelGroup();
+        row.addGroup(componentColumn);
+        
+        groupLayout.setHorizontalGroup(row);
+        
+        GroupLayout.SequentialGroup labelAndCompColumn = groupLayout.createSequentialGroup();
+        groupLayout.setVerticalGroup(labelAndCompColumn);
+        
+        int preferredSize = GroupLayout.PREFERRED_SIZE;
+        
+        for(JLabel label : labels) {
+            labelColumn.addComponent(label);
+        }
+        
+        for(Component component : components) {
+            componentColumn.addComponent(component, preferredSize, preferredSize, preferredSize);
+        }
+        
+        for(int i = 0; i < labels.length; i++) {
+            labelAndCompColumn.addGroup(groupLayout.createParallelGroup()
+                    .addComponent(labels[i])
+                    .addComponent(components[i], preferredSize, preferredSize, preferredSize));
+        }
+        
+        return panel;
+    }
+    
+    /*public ExampleGUINo1() {
         frame = new JFrame("Contact Information");
         actionHandler = new ActionButtonHandler();  //Creates the action handler for the action handler 
         FlowLayout flowLayout = new FlowLayout(FlowLayout.CENTER);
+        container = frame.getContentPane();
         panel = new JPanel();
         
         panel.setLayout(flowLayout);
+        container.setLayout(flowLayout);
         container = frame.getContentPane();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //Program closes when user hits close
         frame.setSize(400, 600);
@@ -59,9 +144,28 @@ public class ExampleGUINo1 extends JFrame {
         volunteerOption = new JRadioButton("Would you like to be contacted specifically about volunteer information");
         disregardQuote = new JRadioButton("Prefer not to answer");
         quoteYesOption = new JRadioButton("Yes");
-        quoteNoOption = new JRadioButton("No");              
-                
+        quoteNoOption = new JRadioButton("No");     
+        
+        d1 = new Dimension(300, 300);
+        d2 = new Dimension(100, 50);
+        d3 = new Dimension();
+        d4 = new Dimension();
+        d5 = new Dimension();
+       
+        panel.setPreferredSize(d1);
+        
+        firstName.setPreferredSize(d2);
+        
+        panel.add(firstName , lastName, email, disregardEmail, phoneNum1, phoneNum2, phoneNum3,
+                disregardPhoneNum, street, city, state, zip, disregardAddress, emailOption,
+                textOption, volunteerOption, quote, disregardQuote, quoteYesOption);
+        
+        firstName.setEditable(true);
+        
+        container.add(panel);
+        frame.setVisible(true);
     }
+*/
     
     private class ActionButtonHandler implements ActionListener {
         public void actionPerformed(ActionEvent e) {
@@ -69,6 +173,12 @@ public class ExampleGUINo1 extends JFrame {
         }
     }
     public static void main(String[] args) {
-        new ExampleGUINo1();           //creates a new instantiation of calculator
+//        new ExampleGUINo1();           //creates a new instantiation of calculator
+        JComponent labelsAndComps = setDualColumnFormat(stringLabels, components);
+        JComponent panelContainer = new JPanel(new BorderLayout(5, 5));
+        panelContainer.add(new JLabel("Purchase Form", SwingConstants.CENTER), BorderLayout.PAGE_START);
+        panelContainer.add(labelsAndComps, BorderLayout.CENTER);
+        
+        JOptionPane.showMessageDialog(null, labelsAndComps);
     }
 }
