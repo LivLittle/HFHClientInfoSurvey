7da45ed70da13e35a301d62df16f51f69482759c
LivLittle <olittle0424@gmail.com>
2018-04-22 22:07:56 -0500

    Cleaned up GUI and code

--- ExampleGUINo1/src/exampleguino1/ExampleGUINo1.java
+++ ExampleGUINo1/src/exampleguino1/ExampleGUINo1.java
@@ -20,29 +20,23 @@ import java.awt.*;
 
 public class ExampleGUINo1 extends JFrame {
     
-//    private final ActionButtonHandler actionHandler;  //this action handler is used for ALL the buttons
-//    private JFrame frame;
-//    private Container container;
-//    private JPanel panel;
-//    private Dimension d1, d2, d3, d4, d5;
-//    private JTextField firstName, lastName, email, phoneNum1,phoneNum2, phoneNum3, street, city, state, zip, quote;
-//    private JRadioButton disregardEmail, disregardPhoneNum, disregardAddress, emailOption, textOption, volunteerOption, disregardQuote, quoteYesOption, quoteNoOption;
+    private ActionComponentHandler actionHandler;
     
     static JComponent[] components = {
-        new JTextField(15),
-        new JTextField(15),
-        new JTextField(15),
+        new JTextField(20),
+        new JTextField(20),
+        new JTextField(20),
         new JTextField(10),
         new JRadioButton(),
         new JRadioButton(),
         new JTextField(20),
         new JTextField(20),
-        new JTextField(2),
+        new JTextField(5),
         new JTextField(10),
         new JRadioButton(),
         new JRadioButton(),
         new JRadioButton(),
-        new JTextField(30),
+        new JTextField(20),
         new JRadioButton(),
         new JRadioButton(),
         new JRadioButton()
@@ -67,6 +61,19 @@ public class ExampleGUINo1 extends JFrame {
         "I allow this discription to be quoted:  ",
         "I do not allow this discription to be quoted:  ",
     };
+    
+    public ExampleGUINo1() {
+        JComponent labelsAndComps = setDualColumnFormat(stringLabels, components);
+        JComponent panelContainer = new JPanel(new BorderLayout(5, 5));
+        panelContainer.add(new JLabel("Purchase Form", SwingConstants.CENTER), BorderLayout.PAGE_START);
+        panelContainer.add(labelsAndComps, BorderLayout.CENTER);
+        
+        actionHandler = new ActionComponentHandler();
+        
+        testBasicGUI();
+        
+        JOptionPane.showMessageDialog(null, labelsAndComps);
+    }
     
     public static JComponent setDualColumnFormat(String[] stringLabels, JComponent[] components) {
         JComponent panel = new JPanel();
@@ -110,99 +117,43 @@ public class ExampleGUINo1 extends JFrame {
         return panel;
     }
     
-    /*public ExampleGUINo1() {
-        frame = new JFrame("Contact Information");
-        actionHandler = new ActionButtonHandler();  //Creates the action handler for the action handler 
-        FlowLayout flowLayout = new FlowLayout(FlowLayout.CENTER);
-        container = frame.getContentPane();
-        panel = new JPanel();
+    private static void testBasicGUI() {
         
-        panel.setLayout(flowLayout);
-        container.setLayout(flowLayout);
-        container = frame.getContentPane();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //Program closes when user hits close
-        frame.setSize(400, 600);
-        frame.setResizable(false);
-        
-        firstName = new JTextField("First Name: ");
-        lastName = new JTextField("Last Name: ");
-        email = new JTextField("Email: ");
-        phoneNum1 = new JTextField("Phone Number: ");
-        phoneNum2 = new JTextField("-");
-        phoneNum3 = new JTextField("-");
-        street = new JTextField("Street Name: ");
-        city = new JTextField("City Name: ");
-        state = new JTextField("State: ");
-        zip = new JTextField("Zip: ");
-        quote = new JTextField("What do you like best about ReStore?");
-        
-        disregardEmail = new JRadioButton("Prefer not to answer");
-        disregardPhoneNum = new JRadioButton("Prefer not to answer");
-        disregardAddress = new JRadioButton("Prefer not to answer");
-        emailOption = new JRadioButton("I want email notifications about Restore");
-        textOption = new JRadioButton("I want text notifications about Restore");
-        volunteerOption = new JRadioButton("Would you like to be contacted specifically about volunteer information");
-        disregardQuote = new JRadioButton("Prefer not to answer");
-        quoteYesOption = new JRadioButton("Yes");
-        quoteNoOption = new JRadioButton("No");     
-        
-        d1 = new Dimension(300, 300);
-        d2 = new Dimension(100, 50);
-        d3 = new Dimension();
-        d4 = new Dimension();
-        d5 = new Dimension();
-       
-        panel.setPreferredSize(d1);
-        
-        firstName.setPreferredSize(d2);
-        
-        panel.add(firstName , lastName, email, disregardEmail, phoneNum1, phoneNum2, phoneNum3,
-                disregardPhoneNum, street, city, state, zip, disregardAddress, emailOption,
-                textOption, volunteerOption, quote, disregardQuote, quoteYesOption);
-        
-        firstName.setEditable(true);
-        
-        container.add(panel);
-        frame.setVisible(true);
-    }
-*/
-    
-    private static boolean validateEmail() {
-        
+//        for(Component component : components) {
+//            System.out.print(component.getAccessibleContext());
+//        }
     }
     
-    private static int editPhoneNum() {
-        
-    }
+//    private static boolean validateEmail() {
+//        
+//    }
+//    
+//    private static int editPhoneNum() {
+//        
+//    }
+//    
+//    private static boolean validatePhoneNum() {
+//        
+//    }
+//    
+//    private static boolean validateAddress() {
+//        
+//    }
+//    
+//    private static String editState() {
+//        
+//    }
+//    
+//    private static boolean validateState() {
+//        
+//    }  
     
-    private static boolean validatePhoneNum() {
-        
-    }
-    
-    private static boolean validateAddress() {
-        
-    }
-    
-    private static String editState() {
-        
-    }
-    
-    private static boolean validateState() {
-        
-    }
-    
-    private class ActionButtonHandler implements ActionListener {
+    private class ActionComponentHandler implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             
         }
     }
     public static void main(String[] args) {
-//        new ExampleGUINo1();           //creates a new instantiation of calculator
-        JComponent labelsAndComps = setDualColumnFormat(stringLabels, components);
-        JComponent panelContainer = new JPanel(new BorderLayout(5, 5));
-        panelContainer.add(new JLabel("Purchase Form", SwingConstants.CENTER), BorderLayout.PAGE_START);
-        panelContainer.add(labelsAndComps, BorderLayout.CENTER);
-        
-        JOptionPane.showMessageDialog(null, labelsAndComps);
+        new ExampleGUINo1();           //creates a new instantiation of calculator
     }
 }
